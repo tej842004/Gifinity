f08273afda7c41c5af27bf1d921bce18365317e2
Prashant Chauhan <imtej08@gmail.com>
2025-05-21 22:38:12 +0530

    add a scrollbar to the tags

--- src/App.tsx
+++ src/App.tsx
@@ -15,21 +15,23 @@ const App = () => {
   return (
     <Grid
       templateAreas={{
-        base: `"nav" "main"`,
+        base: `"nav" "aside" "main"`,
         md: `"nav nav" "aside aside" "main main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        md: "1fr",
       }}
     >
       <GridItem area="nav">
         <NavBar onSearch={(search) => setGifQuery({ ...gifQuery, search })} />
       </GridItem>
-      <Show above="lg">
-        <GridItem as="aside">
-          <Tags
-            onSelectTag={(tag) => setGifQuery({ ...gifQuery, tag })}
-            selectedTag={gifQuery.tag}
-          />
-        </GridItem>
-      </Show>
+      <GridItem as="aside">
+        <Tags
+          onSelectTag={(tag) => setGifQuery({ ...gifQuery, tag })}
+          selectedTag={gifQuery.tag}
+        />
+      </GridItem>
       <GridItem area="main">
         <GifGrid gifQuery={gifQuery} />
       </GridItem>
@@ -38,3 +40,58 @@ const App = () => {
 };
 
 export default App;
+// import { Box } from "@chakra-ui/react";
+// import React from "react";
+// import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+// import "react-horizontal-scrolling-menu/dist/styles.css";
+
+// const items = Array.from({ length: 20 }, (_, i) => ({
+//   id: `item-${i}`,
+//   title: `Item ${i}`,
+// }));
+
+// function Card({ itemId, title }: { itemId: string; title: string }) {
+//   return (
+//     <div
+//       style={{
+//         minWidth: "160px",
+//         margin: "0 10px",
+//         padding: "20px",
+//         background: "#eee",
+//         textAlign: "center",
+//         borderRadius: "8px",
+//       }}
+//       itemID={itemId}
+//     >
+//       {title}
+//     </div>
+//   );
+// }
+
+// const LeftArrow = () => {
+//   const { scrollPrev } = React.useContext(VisibilityContext);
+//   return <button onClick={() => scrollPrev()}>{"<"}</button>;
+// };
+
+// const RightArrow = () => {
+//   const { scrollNext } = React.useContext(VisibilityContext);
+//   return <button onClick={() => scrollNext()}>{">"}</button>;
+// };
+
+// const HorizontalScroll = () => {
+//   return (
+//     <Box width="80vw" mx="auto">
+//       <ScrollMenu
+//         LeftArrow={<LeftArrow />}
+//         RightArrow={<RightArrow />}
+//         wrapperClassName="hide-scrollbar"
+//       >
+//         {items.map(({ id, title }) => (
+//           <Card itemId={id} title={title} key={id} />
+//         ))}
+//       </ScrollMenu>
+//     </Box>
+//   );
+// };
+
+// export default HorizontalScroll;
--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -1,4 +1,8 @@
-import { Button, Stack, Text } from "@chakra-ui/react";
+import { Box, Button, IconButton, Text } from "@chakra-ui/react";
+import React from "react";
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+import "react-horizontal-scrolling-menu/dist/styles.css";
+import { TfiAngleDoubleLeft, TfiAngleDoubleRight } from "react-icons/tfi";
 import { tags } from "../data/tags";
 
 interface Props {
@@ -6,24 +10,50 @@ interface Props {
   selectedTag: string;
 }
 
+const LeftArrow = () => {
+  const { scrollPrev } = React.useContext(VisibilityContext);
+  return (
+    <IconButton
+      aria-label="double left button"
+      icon={<TfiAngleDoubleLeft />}
+      variant="ghost"
+      onClick={() => scrollPrev()}
+    />
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = React.useContext(VisibilityContext);
+  return (
+    <IconButton
+      aria-label="double right button"
+      icon={<TfiAngleDoubleRight />}
+      variant="ghost"
+      onClick={() => scrollNext()}
+    />
+  );
+};
+
 const Tags = ({ onSelectTag, selectedTag }: Props) => {
   return (
-    <Stack
-      direction="row"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-    >
-      {tags.map((tag) => (
-        <Button
-          variant={tag.name === selectedTag ? "solid" : "outline"}
-          key={tag.id}
-          onClick={() => onSelectTag(tag.name)}
-        >
-          <Text fontSize="md">{tag.name}</Text>
-        </Button>
-      ))}
-    </Stack>
+    <Box width="80vw" mx="auto">
+      <ScrollMenu
+        LeftArrow={<LeftArrow />}
+        RightArrow={<RightArrow />}
+        wrapperClassName="hide-scrollbar"
+      >
+        {tags.map((tag) => (
+          <Button
+            variant={tag.name === selectedTag ? "solid" : "outline"}
+            key={tag.id}
+            onClick={() => onSelectTag(tag.name)}
+            mx={1}
+          >
+            <Text fontSize="md">{tag.name}</Text>
+          </Button>
+        ))}
+      </ScrollMenu>
+    </Box>
   );
 };
 
