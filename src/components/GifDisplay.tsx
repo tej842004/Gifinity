6c06e898fe0dfcc02defbe6e244455109356d7c9
Prashant Chauhan <imtej08@gmail.com>
2025-05-29 22:38:56 +0530

    Improve the gif detail page

--- src/components/GifDisplay.tsx
+++ src/components/GifDisplay.tsx
@@ -1,4 +1,4 @@
-import { Image, Button, Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 
 interface GifDisplayProps {
   imageUrl: string;
@@ -14,17 +14,32 @@ const GifDisplay = ({
   onDownload,
 }: GifDisplayProps) => {
   return (
-    <div>
-      <Image src={imageUrl} />
-      <Box display="flex" alignItems="center" justifyContent="center" my={1}>
+    <Box
+      display="flex"
+      flexDirection="column"
+      gap={2}
+      width="auto"
+      height="300px"
+      position="relative"
+    >
+      <Box
+        as="img"
+        src={imageUrl}
+        height="100%"
+        width="100%"
+        objectFit="cover"
+        borderRadius="md"
+      />
+      <Box>
         <Button
+          width="100%"
           variant="outline"
           onClick={() => onDownload(downloadUrl, fileName)}
         >
           Download
         </Button>
       </Box>
-    </div>
+    </Box>
   );
 };
 
